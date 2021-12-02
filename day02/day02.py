929f04d1f4783bb7aecc9f9096dd3839c72d796a
deshudiosh <deshudiosh@gmail.com>
2021-12-02 14:31:57 +0100

    day2 first done

--- day02/day02.py
+++ day02/day02.py
@@ -5,4 +5,5 @@ for d, v in zip(dirs, [int(v) for v in vals]):
     if d == 'forward': forward += v
     else: depth += v if d == "down" else -v
 
-print(forward, depth, forward*depth)
+print(forward*depth)
+

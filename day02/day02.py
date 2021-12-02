37f9e0c233d407e5d774596c9f2cb610c23515c1
deshudiosh <deshudiosh@gmail.com>
2021-12-02 14:57:10 +0100

    day2 both done

--- day02/day02.py
+++ day02/day02.py
@@ -1,3 +1,4 @@
+# part 1
 dirs, vals = zip(*(x.split() for x in open('./input.txt', 'r').readlines()))
 
 forward = depth = 0
@@ -6,4 +7,18 @@ for d, v in zip(dirs, [int(v) for v in vals]):
     else: depth += v if d == "down" else -v
 
 print(forward*depth)
+
+
+# part 2
+aims, vals = zip(*(x.split() for x in open('./input.txt', 'r').readlines()))
+
+forward = depth = aim = 0
+for a, v in zip(aims, [int(v) for v in vals]):
+    if a == 'forward':
+        forward += v
+        depth += v * aim
+    else: aim += v if a == "down" else -v
+
+print(forward*depth)
+
 

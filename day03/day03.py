ba60e12d3ba23ac56a22599c760e1b2b1f901ecf
deshudiosh <deshudiosh@gmail.com>
2021-12-03 15:09:07 +0100

    day3, second not working

--- day03/day03.py
+++ day03/day03.py
@@ -7,6 +7,27 @@ gamma_binary = "".join(Counter(li).most_common(1)[0][0] for li in zip(*[list(x) 
 epsilon_binary = "".join(["1" if i == "0" else "0" for i in list(gamma_binary)])
 gamma, epsilon = map(lambda x: int(x, 2), [gamma_binary, epsilon_binary])
 
-print(gamma*epsilon)
+print(gamma*epsilon, "\n")
 
+
+# part2
+num_bits = len(data[0])
+
+# oxygen
+oxygen = data.copy()
+for i in range(num_bits):
+    bits = list(zip(*[list(x) for x in oxygen]))[i]
+    counter = Counter(bits).most_common()
+    keep_bit = counter[0][0] if counter[0][1] > counter[1][1] else 1  # sure?
+    # print(counter, keep_bit)
+
+    cpy = oxygen.copy()
+    for o in cpy:
+        if o[i] != keep_bit:
+            cpy.remove(o)
+
+    oxygen = cpy
+    print(len(oxygen))
+
+# print(oxygen)
 

ca49e5311b1062f4996a56dfa52c86f28012f14b
deshudiosh <deshudiosh@gmail.com>
2021-12-03 13:04:05 +0100

    day3 1/2

--- day03/day03.py
+++ day03/day03.py
@@ -3,7 +3,7 @@ from collections import Counter
 data = [d.strip() for d in open('./input.txt', 'r').readlines()]
 
 # part1
-gamma_binary = "".join((Counter(li).most_common(1)[0][0] for li in (zip(*[list(x) for x in data]))))
+gamma_binary = "".join(Counter(li).most_common(1)[0][0] for li in zip(*[list(x) for x in data]))
 epsilon_binary = "".join(["1" if i == "0" else "0" for i in list(gamma_binary)])
 gamma, epsilon = map(lambda x: int(x, 2), [gamma_binary, epsilon_binary])
 

781477f7bc413f68911a758481b724e7b428c755
deshudiosh <deshudiosh@gmail.com>
2021-12-05 02:19:34 +0100

    day4

--- day04/day04.py
+++ day04/day04.py
@@ -1,8 +1,5 @@
-import functools
 import math
-import operator
 from dataclasses import dataclass
-from pprint import pprint
 
 
 @dataclass
@@ -12,60 +9,57 @@ class Field:
 
     # logging purpose only
     def __repr__(self):
-        return str(self.value)
+        return str(self.value) if not self.marked else f'({self.value})'
 
 
 class Board:
     def __init__(self, rows_data):
         self.fields = [Field(int(d)) for d in " ".join(rows_data).split(" ") if d]
+        self.did_bingo_already = False
 
-    def mark_field(self, num: int):
+    def bingo(self, num) -> bool:
+        # mark the fields if found
         for field in self.fields:
             if field.value == num:
                 field.marked = True
 
-    def is_won(self) -> bool:
-        is_won = False
-
+        # check if bingo
         split = int(math.sqrt(len(self.fields)))
-        _rows = [self.fields[i:i + split] for i in range(0, len(self.fields), split)]
-        _cols = [list(z) for z in zip(*rows)]  # invert rows to columns
+        board_rows = [self.fields[i:i + split] for i in range(0, len(self.fields), split)]
+        board_cols = [list(z) for z in zip(*board_rows)]  # rezip rows to columns
 
-        for line in _rows + _cols:
+        for line in (board_rows + board_cols):
             if all([field.marked for field in line]):
-                is_won = True
+                self.did_bingo_already = True
+                break
 
-        return is_won
+        return self.did_bingo_already
 
     def sum_of_unmarked(self) -> int:
         return sum([field.value for field in self.fields if not field.marked])
 
 
-data = [d.strip() for d in open('./input.txt', 'r').readlines()]
-numbers = [int(x) for x in data.pop(0).split(',')]
+if __name__ == '__main__':
+    data = [d.strip() for d in open('./input.txt', 'r').readlines()]
+    numbers = [int(x) for x in data.pop(0).split(',')]
 
-# create boards
-boards = []
-rows = []
-for d in [x for x in data if x]:
-    rows.append(d)
-    if len(rows) == 5:
-        boards.append(Board(rows))
-        rows = []
+    # create boards
+    boards = []
+    rows = []
+    for d in [x for x in data if x]:
+        rows.append(d)
+        if len(rows) == 5:
+            boards.append(Board(rows))
+            rows = []
 
+    # find the scores
+    scores = []
+    for n in numbers:
+        for b in [b for b in boards if not b.did_bingo_already]:
+            if b.bingo(n):
+                scores.append(n * b.sum_of_unmarked())
 
-# find the winner
-scores = []
-for n in numbers:
-    for b in boards:
-        b.mark_field(n)
-        if b.is_won():
-            boards.remove(b)
-            scores.append(n * b.sum_of_unmarked())
-
-
-print(f"First part: {scores[0]}")
-print(f"Second part: {scores[-1]}")
+    print(f"First part: {scores[0]}\nSecond part: {scores[-1]}")
 
 
 

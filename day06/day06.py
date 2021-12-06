f1bc0dd4e698605ba2ca9e735321ee507fe7291d
deshudiosh <deshudiosh@gmail.com>
2021-12-06 15:47:46 +0100

    day6 done

--- day06/day06.py
+++ day06/day06.py
@@ -2,7 +2,7 @@ from collections import Counter
 from typing import List
 
 
-def solve(d, days):
+def solve(d, days, msg: str):
     fishes = Counter(d)
 
     for day in range(days):
@@ -17,25 +17,15 @@ def solve(d, days):
             else:
                 after_day_fishes[fish] += count
 
-
-        # for f in range(len(d)):
-        #     fish = d[f]
-        #     fish -= 1
-        #     if fish < 0:
-        #         fish = 6
-        #         d.append(8)
-        #     d[f] = fish
-
         fishes = after_day_fishes
 
-    print(fishes)
-    total_count = sum(fishes.values())
-    print(total_count)
+    print(f'{msg} {sum(fishes.values())}')
 
 
 if __name__ == '__main__':
-    data = [int(x) for x in open('./test.txt', 'r').read().split(',')]
-    solve(data, 18)
+    data = [int(x) for x in open('./input.txt', 'r').read().split(',')]
+    solve(data, 80, 'puzzle1:')
+    solve(data, 256, 'puzzle2:')
 
 
 

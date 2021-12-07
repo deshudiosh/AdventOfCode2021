0956f60d1d8877842f992a7242331deec29ff264
deshudiosh <deshudiosh@gmail.com>
2021-12-07 09:09:30 +0100

    day7 done

---
+++ day07/day07.py
@@ -0,0 +1,36 @@
+def fuel_to_align_constant(crab_pos_list):
+    possible_alignments = range(min(crab_pos_list), max(crab_pos_list)+1)
+    fuel_usages = []
+
+    for p_a in possible_alignments:
+        fuel = 0
+        for pos in crab_pos_list:
+            fuel += abs(pos - p_a)
+
+        fuel_usages.append(fuel)
+
+    return sorted(fuel_usages)[0]
+
+
+def fuel_to_align_incremental(crab_pos_list):
+    possible_alignments = range(min(crab_pos_list), max(crab_pos_list)+1)
+    fuel_usages = []
+
+    for p_a in possible_alignments:
+        fuel = 0
+
+        for pos in crab_pos_list:
+            num_steps = abs(pos - p_a)
+            for i in range(num_steps):
+                fuel += i+1
+
+        fuel_usages.append(fuel)
+
+    return sorted(fuel_usages)[0]
+
+
+if __name__ == '__main__':
+    data = [int(x) for x in open('./input.txt', 'r').read().split(',')]
+    # data = list(map(int, "16,1,2,0,4,2,7,1,2,14".split(',')))
+    print(f'Puzzle1: {fuel_to_align_constant(data)}')
+    print(f'Puzzle2: {fuel_to_align_incremental(data)}')
